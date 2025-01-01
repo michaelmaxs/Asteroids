6c344242803ae664f90df7040ffdd37848830451
michaelmaxs <michael.max.schaefer@gmail.com>
2025-01-01 14:58:14 +0100

    added clock.tick() to limit framerate

--- main.py
+++ main.py
@@ -12,7 +12,10 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")   
     
     pygame.init()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -22,6 +25,7 @@ def main():
         screen.fill(color="black")
         pygame.display.flip()
 
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
