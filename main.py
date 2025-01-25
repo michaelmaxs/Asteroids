2428a6f27afc88af762694995d27d4d5299c209b
michaelmaxs <michael.max.schaefer@gmail.com>
2025-01-25 23:42:48 +0100

    collision logic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 from constants import *
 from player import *
@@ -36,6 +37,11 @@ def main():
 
         for obj in updatable:
             obj.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()            
 
         screen.fill(color="black")
 
